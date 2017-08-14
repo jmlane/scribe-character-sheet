085dcbe9de86909513d5d6eb6e0d920894d4eb99
Jonathan M. Lane <jmlane@gmail.com>
2017-08-14 13:18:59 -0300

    Added Elm Architecture `Html.program` stub

---
+++ src/Main.elm
@@ -0,0 +1,46 @@
+module Main exposing (main)
+
+import Html exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    section []
+        [ h1 [] [ text "Character" ]
+        , text "…"
+        ]
