3773435987617422fdbd8adf98f10482a5a49edb
Jonathan M. Lane <jmlane@gmail.com>
2019-04-29 20:13:36 -0300

    Start changing model to Genesys system

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (main)
 
-import Browser exposing (document)
+import Browser
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 main : Program () Model Msg
@@ -14,18 +15,54 @@ main =
         }
 
 
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , archetype : Maybe Archetype
+    , career : String
+    , player : String
+    }
+
+
+type Archetype
+    = AverageHuman
+    | Laborer
+    | Intellectual
+    | Aristocrat
+
+
+toString : Archetype -> String
+toString archetype =
+    case archetype of
+        AverageHuman ->
+            "Average Human"
+
+        Laborer ->
+            "Laborer"
+
+        Intellectual ->
+            "Intellectual"
+
+        Aristocrat ->
+            "Aristocrat"
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { name = "Laura Sauer"
-      , player = "Jon"
-      , classes = [ ( "Paladin", 7 ) ]
-      , race = "Human"
-      , attributes = Attributes 18 12 14 8 8 18
-      , inspiration = False
-      , skills = []
-      }
+    ( Model "" Nothing "" ""
     , Cmd.none
     )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -35,142 +72,51 @@ update msg model =
             ( model, Cmd.none )
 
 
+
+-- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
 
 
--- MODELS
-
-
-type Msg
-    = NoOp
-
-
-type alias Model =
-    { name : String
-    , player : String
-    , classes : List ( String, Int )
-    , race : String
-    , attributes : Attributes
-    , inspiration : Bool
-    , skills : Skills
-    }
-
-
-
--- hp (total, current, temp) note: what's derived vs provided
--- hit dice
--- attacks/spells
-
-
-type alias Attributes =
-    { strength : Int
-    , dexterity : Int
-    , constitution : Int
-    , intelligence : Int
-    , wisdom : Int
-    , charisma : Int
-    }
-
-
-type alias Skills =
-    List String
-
-
-
--- VIEWS
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
+    div []
+        [ viewCharacterInfo model
+        ]
+
+
+viewCharacterInfo : Model -> Html Msg
+viewCharacterInfo model =
     let
-        subheader : String
-        subheader =
-            model.race ++ " " ++ characterLevels
+        labelledInput : String -> String -> Html Msg
+        labelledInput label_ value_ =
+            label []
+                [ text label_
+                , input [ value value_ ] []
+                ]
 
-        characterLevels : String
-        characterLevels =
-            model.classes
-                |> List.map
-                    (\( class, level ) ->
-                        "Level " ++ String.fromInt level ++ " " ++ class
-                    )
-                |> String.join ", "
+        archetypeString : Maybe Archetype -> String
+        archetypeString maybe =
+            case maybe of
+                Just archetype ->
+                    toString archetype
 
-        inspiration : String
-        inspiration =
-            if model.inspiration then
-                "true"
-
-            else
-                "false"
+                Nothing ->
+                    ""
     in
-    section []
-        [ h1 []
-            [ text model.name
-            , br [] []
-            , text subheader
+    div []
+        [ h2 [] [ text "Character" ]
+        , div []
+            [ labelledInput "Character Name:" model.name
+            , labelledInput "Species/Archetypes:" (archetypeString model.archetype)
+            , labelledInput "Career:" model.career
+            , labelledInput "Player:" model.player
             ]
-        , p [] [ text ("Player: " ++ model.player) ]
-        , p [] [ text ("Race: " ++ model.race) ]
-        , p []
-            [ text ("Inspiration: " ++ inspiration) ]
-        , viewAttributes model.attributes
-        , viewSkills model.skills
-        ]
-
-
-viewAttributes : Attributes -> Html Msg
-viewAttributes attributes =
-    let
-        mapToHtmlLi : ( String, Int ) -> Html Msg
-        mapToHtmlLi ( attribute, value ) =
-            li [] [ text <| attribute ++ ": " ++ String.fromInt value ]
-    in
-    section []
-        [ h2 [] [ text "Attributes" ]
-        , ul [] <|
-            List.map mapToHtmlLi <|
-                [ ( "Strength", attributes.strength )
-                , ( "Dexterity", attributes.dexterity )
-                , ( "Constitution", attributes.constitution )
-                , ( "Intelligence", attributes.intelligence )
-                , ( "Wisdom", attributes.wisdom )
-                , ( "Charisma", attributes.charisma )
-                ]
-        ]
-
-
-viewSkills : Skills -> Html Msg
-viewSkills skills =
-    let
-        mapfunc : String -> Html Msg
-        mapfunc name =
-            li [] [ text name ]
-    in
-    section []
-        [ h2 [] [ text "Skills" ]
-        , ul [] <|
-            List.map mapfunc <|
-                [ "Athletics"
-                , "Acrobatics"
-                , "Sleight of Hand"
-                , "Stealth"
-                , "Arcana"
-                , "History"
-                , "Investigation"
-                , "Nature"
-                , "Religion"
-                , "Animal Handling"
-                , "Insight"
-                , "Medicine"
-                , "Perception"
-                , "Survival"
-                , "Deception"
-                , "Intimidation"
-                , "Performance"
-                , "Persuasion"
-                ]
         ]
