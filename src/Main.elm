48c656f80d9d5e2fed6e2b335a2a334f7a304ca0
Jonathan M. Lane <jmlane@gmail.com>
2019-05-01 02:51:55 -0300

    Characteristics and Derived Attributes

--- src/Main.elm
+++ src/Main.elm
@@ -24,6 +24,24 @@ type alias Model =
     , archetype : Maybe Archetype
     , career : String
     , player : String
+    , soak : Int
+    , woundsThreshold : Int
+    , woundsCurrent : Int
+    , strainThreshold : Int
+    , strainCurrent : Int
+    , defenseRanged : Int
+    , defenseMelee : Int
+    , characteristics : Characteristics
+    }
+
+
+type alias Characteristics =
+    { brawn : Int
+    , agility : Int
+    , intellect : Int
+    , cunning : Int
+    , willpower : Int
+    , presence : Int
     }
 
 
@@ -52,7 +70,7 @@ toString archetype =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model "" Nothing "" ""
+    ( Model "" Nothing "" "" 0 0 0 0 0 0 0 (Characteristics 0 0 0 0 0 0)
     , Cmd.none
     )
 
@@ -89,19 +107,22 @@ view : Model -> Html Msg
 view model =
     div []
         [ viewCharacterInfo model
+        , viewDerivedAttributes model
+        , viewCharacteristics model.characteristics
+        ]
+
+
+labelledInput : String -> String -> Html Msg
+labelledInput label_ value_ =
+    label []
+        [ text label_
+        , input [ value value_ ] []
         ]
 
 
 viewCharacterInfo : Model -> Html Msg
 viewCharacterInfo model =
     let
-        labelledInput : String -> String -> Html Msg
-        labelledInput label_ value_ =
-            label []
-                [ text label_
-                , input [ value value_ ] []
-                ]
-
         archetypeString : Maybe Archetype -> String
         archetypeString maybe =
             case maybe of
@@ -111,12 +132,37 @@ viewCharacterInfo model =
                 Nothing ->
                     ""
     in
-    div []
+    section []
         [ h2 [] [ text "Character" ]
-        , div []
-            [ labelledInput "Character Name:" model.name
-            , labelledInput "Species/Archetypes:" (archetypeString model.archetype)
-            , labelledInput "Career:" model.career
-            , labelledInput "Player:" model.player
-            ]
+        , labelledInput "Character Name:" model.name
+        , labelledInput "Species/Archetypes:" (archetypeString model.archetype)
+        , labelledInput "Career:" model.career
+        , labelledInput "Player:" model.player
+        ]
+
+
+viewDerivedAttributes : Model -> Html Msg
+viewDerivedAttributes model =
+    section []
+        [ h2 [] [ text "Derived Attributes" ]
+        , labelledInput "Soak Value" (String.fromInt model.soak)
+        , labelledInput "Wound Threshold" (String.fromInt model.woundsThreshold)
+        , labelledInput "Current Wounds" (String.fromInt model.woundsCurrent)
+        , labelledInput "Strain Threshold" (String.fromInt model.strainThreshold)
+        , labelledInput "Current Strain" (String.fromInt model.strainCurrent)
+        , labelledInput "Ranged Defense" (String.fromInt model.defenseRanged)
+        , labelledInput "Melee Defense" (String.fromInt model.defenseMelee)
+        ]
+
+
+viewCharacteristics : Characteristics -> Html Msg
+viewCharacteristics characteristics =
+    section []
+        [ h2 [] [ text "Characteristics" ]
+        , labelledInput "Brawn" (String.fromInt characteristics.brawn)
+        , labelledInput "Agility" (String.fromInt characteristics.agility)
+        , labelledInput "Intellect" (String.fromInt characteristics.intellect)
+        , labelledInput "Cunning" (String.fromInt characteristics.cunning)
+        , labelledInput "Willpower" (String.fromInt characteristics.willpower)
+        , labelledInput "Presence" (String.fromInt characteristics.presence)
         ]
